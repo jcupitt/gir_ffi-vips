d809b15afe096321294497019d969dcde4f72cc5
John Cupitt <jcupitt@gmail.com>
2014-11-25 14:49:27 +0000

    start a full vips8 binding

---
+++ vips8.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+# gem install gir_ffi
+
+require 'gir_ffi'
+
+GirFFI.setup :Vips
+
+# see this to see how to define overrides
+# https://github.com/mvz/gir_ffi-gtk/blob/master/lib/gir_ffi-gtk/base.rb
