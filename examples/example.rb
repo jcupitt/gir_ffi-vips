796504f2441c06a2fa1d8330164e8d5b7e0761ac
John Cupitt <jcupitt@gmail.com>
2015-01-05 17:42:50 +0000

    try to get ref counting working

--- examples/example.rb
+++ examples/example.rb
@@ -2,27 +2,32 @@
 
 require 'vips8'
 
+Vips::cache_set_max 0
+
 if ARGV.length < 2
     raise "usage: #{$PROGRAM_NAME}: input-file output-file"
 end
 
-# we don't need random access to this image, we will just process 
-# top to bottom
-im = Vips::Image.new_from_file ARGV[0], :access => :sequential
+(1..200).each do |i|
+    puts "loop #{i}"
 
-# multiply the green channel by 2
-im *= [1, 2, 1]
+    # we don't need random access to this image, we will just process 
+    # top to bottom
+    im = Vips::Image.new_from_file ARGV[0], :access => :sequential
 
-# make a convolution mask
-mask = Vips::Image.new_from_array [
-        [-1, -1, -1],
-        [-1, 16, -1],
-        [-1, -1, -1]], 8
+    # multiply the green channel by 2
+    im *= [1, 2, 1]
 
-# convolve the image with the mask
-im = im.conv mask
+    # make a convolution mask
+    mask = Vips::Image.new_from_array [
+            [-1, -1, -1],
+            [-1, 16, -1],
+            [-1, -1, -1]], 8
 
-# write back to the filesystem
-im.write_to_file ARGV[1]
+    # convolve the image with the mask
+    im = im.conv mask
 
+    # write back to the filesystem
+    im.write_to_file ARGV[1]
 
+end
