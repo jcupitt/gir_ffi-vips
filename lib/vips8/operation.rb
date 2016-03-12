1a737d0588a4b336b04ce2dd38cd75245602a52d
John Cupitt <jcupitt@gmail.com>
2016-03-12 17:29:07 +0000

    add an override for operation.new
    since it makes a subclass rather than an actual VipsOperation
    
    see https://github.com/mvz/gir_ffi/issues/70

--- lib/vips8/operation.rb
+++ lib/vips8/operation.rb
@@ -1,12 +1,24 @@
 module Vips
 
     class Operation
+
+        # we need to override new: Vips::Operation.new will return a subclass of
+        # operation, not an operation itself
+        setup_method :new
+        def self.new(name)
+            _v2 = GirFFI::InPointer.from :utf8, name
+            _v3 = Vips::Lib.vips_operation_new _v2
+            _v1 = Vips::Operation.wrap(_v3)
+
+            return _v1
+        end
+
         # Fetch arg list, remove boring ones, sort into priority order.
         def get_args
             object_class = GObject::object_class_from_instance self
             io_bits = Vips::ArgumentFlags[:input] | Vips::ArgumentFlags[:output]
             props = object_class.list_properties.select do |prop|
-                flags = get_argument_flags prop.name
+                flags = get_argument_flags prop.get_name
                 flags = Vips::ArgumentFlags.to_native flags, 1
                 (flags & io_bits != 0) &&
                     (flags & Vips::ArgumentFlags[:deprecated] == 0)
